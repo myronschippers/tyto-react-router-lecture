1c6df8f12d688fb3fc8b81035b965a3ee4ecc04e
Myron <dubtwee@gmail.com>
2019-12-09 08:59:47 -0600

    initial codebase

---
+++ src/Components/App/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Animals from '../Animals/Animals.js'
+import Plants from '../Plants/Plants.js'
+import Home from '../Home/Home.js'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       
+      </div>
+    );
+  }
+}
+
+export default App;
