e5772c61cec2e6b681260efe7fc99af27d80aedd
Myron <dubtwee@gmail.com>
2019-12-09 09:13:28 -0600

    altering App component starting content to actually go through component creation

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import Animals from '../Animals/Animals.js'
-import Plants from '../Plants/Plants.js'
-import Home from '../Home/Home.js'
-
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-       
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header>
+                    <h1>Single Page Application (SPA)</h1>
+                </header>
+
+                <div>
+                    <h2>HOME</h2>
+                    <p>Some page content to be shown on the home page of our application.</p>
+                </div>
+
+                <div>
+                    <h2>PLANTS</h2>
+                    <p>What do we have here...? Ooooooo, some glorious plants.</p>
+                </div>
+
+                <div>
+                    <h2>ANIMALS</h2>
+                    <p>YES!!! I love animals. What kind of adorable animals do we get to see?</p>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
